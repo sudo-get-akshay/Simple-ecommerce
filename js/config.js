2b488b05c0b4ce382ec67375665415dd20cf79e3
Akshay Kumar <akshaykumar101104@outlook.com>
2026-07-12 20:26:19 +0530

    chore: Update Clerk Publishable Key and add gitignore

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
 // Replace CLERK_PUBLISHABLE_KEY with your own Publishable Key from the Clerk Dashboard.
 
 export const CONFIG = {
-  CLERK_PUBLISHABLE_KEY: "pk_test_Y2xlYW4tbWFzdG9kb24tMjguY2xlcmsuYWNjb3VudHMuZGV2JA"
+  CLERK_PUBLISHABLE_KEY: "pk_test_Y2VudHJhbC1yYWNlci03OS5jbGVyay5hY2NvdW50cy5kZXYk"
 };
